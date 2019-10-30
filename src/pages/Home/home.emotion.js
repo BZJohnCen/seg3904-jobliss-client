30e062317191a21ab725d6e40b13c34e23ab3327
John Cen <john.cen397@gmail.com>
2019-10-30 03:07:20 -0400

    Added emotion styles to home.js pageview

--- src/pages/Home/home.emotion.js
+++ src/pages/Home/home.emotion.js
@@ -1,4 +1,31 @@
 import { css } from '@emotion/core'
 
-export const Container = css`
+export const CityInput = css`
+    width: 11%;
+    margin-right: 1em;
+`
+
+export const SearchBar = css`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+`
+
+export const SubmitSearch = css`
+    font-size: 0.83em;
+    width: 115%;
+    height: 1.4em;
+    border-radius: 2em;
+    padding: 0.3em 1em 1.5em 1em;
+    font-weight: 200;
+    color: white;
+    background: none;
+
+    &:hover {
+        color: lightgreen;
+        border-color: lightgreen;
+        cursor: pointer;
+    }
 `

59f0fa89f9d43e52a354b7cd14cfaf6b5d65c9aa
John Cen <john.cen397@gmail.com>
2019-10-30 03:07:35 -0400

    Applies emotion styles for home page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import * as styles from './home.emotion'
 
 const Home = () => {
     let [userQuery, setUserQuery] = useState("")
@@ -8,10 +9,10 @@ const Home = () => {
     return (
         <span>
             <h1 style={{ marginTop: "-3em" }}>Jobliss.</h1>
-            <div id="homepage-searchbar">
+            <div css={styles.SearchBar}>
                 <input type="text" placeholder="Enter Job Position, Title, Role, etc"></input>
-                <input id="cityInput" type="text" placeholder="City/State"></input>
-                <Link to="/search"><button id="searchSubmitBtn" type="submit">Search</button></Link>
+                <input css={styles.CityInput} type="text" placeholder="City/State"></input>
+                <Link to="/search"><button css={styles.SubmitSearch} type="submit">Search</button></Link>
             </div>
         </span>
     )

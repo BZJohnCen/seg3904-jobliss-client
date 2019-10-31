6e5612da0e3b5ca172b04906097c81222c7aefa4
John Cen <john.cen397@gmail.com>
2019-10-31 03:18:28 -0400

    Added labels to search input elements, temporary

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -18,19 +18,21 @@ const Home = () => {
     return (
         <div css={styles.HomeContainer}>
             <div style={{ marginTop: "-20%", width: "100%" }}>
-                <h1 css={styles.Title}>Jobliss.</h1>
+                <h1 css={styles.Title}>Jobliss. <span style={{ fontSize: "28px", color: "lightgreen" }}>find yours today</span></h1>
                 <div css={styles.SearchBar}>
                     <input css={styles.QueryInput} 
                         onChange={e => setUserQuery(e.target.value)} 
                         type="text" 
                         placeholder="Type: 'Software Developer' or 'Business Analyst'"
                         onKeyPress={handleEnterKey} />
+                    <span css={styles.LabelSpan} style={{ left: "21.8%" }}>Keyword</span>
                     <input css={styles.QueryInput} 
                         onChange={e => setUserLocation(e.target.value)} 
                         style={{ width: "15%", marginRight: "2em" }} 
                         type="text" 
                         placeholder="Ex. 'Toronto, ON'"
                         onKeyPress={handleEnterKey} />
+                    <span css={styles.LabelSpan} style={{ left: "55.1%" }}>Location</span>
                     <Link to={`/search?query=${userQuery}&location=${userLocation}`}>
                         <button css={styles.SubmitSearch} type="text">Search</button>
                     </Link>

990063955d0dab9008d73f113f24974242c11687
John Cen <john.cen397@gmail.com>
2019-10-31 01:38:05 -0400

    Added styles for the search-results page view

--- src/pages/Results/search-results.emotion.js
+++ src/pages/Results/search-results.emotion.js
@@ -1,4 +1,16 @@
 import { css } from '@emotion/core'
 
-export const Container = css`
+export const SearchResultsContainer = css`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+`
+
+export const ResultsContent = css`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
 `

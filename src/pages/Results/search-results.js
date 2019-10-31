01f1eb360d5f8cd0b27f030f29cec52392cfde30
John Cen <john.cen397@gmail.com>
2019-10-31 03:18:56 -0400

    Included a back navigation button

--- src/pages/Results/search-results.js
+++ src/pages/Results/search-results.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 import { Row, Col, List } from 'antd'
 import axios from 'axios'
 import * as styles from './search-results.emotion'
-import { useLocation } from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom'
 import IndeedListItem from '../../components/IndeedListItem/indeed-list-item'
 
 const useQuery = () => {
@@ -40,7 +40,12 @@ const SearchResults = (props) => {
         <div css={styles.SearchResultsContainer}>
             <Row>
                 <Col md={{ span: 22, offset: 1 }} xs={{ span: 24 }}>
-                    <h2 style={{ fontWeight: "100", marginTop: '7%', color: "white" }}>Search Results</h2>
+                    <div css={styles.HeaderContainer}>
+                        <Link to="/" style={{ position: "absolute", left: "0", bottom: "10%" }}>
+                            <svg width="60" height="35" viewBox="1 0 22 24" fill="white" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm-4.828 11.5l4.608 3.763-.679.737-6.101-5 6.112-5 .666.753-4.604 3.747h11.826v1h-11.828z"/></svg>
+                        </Link>
+                        <h2 style={{ fontWeight: "100", marginTop: '7%', color: "white" }}>Search Results</h2>
+                    </div>
                     <div css={styles.ResultsContent}>
                         {indeedResults ?
                             <List
@@ -49,7 +54,7 @@ const SearchResults = (props) => {
                                 dataSource={indeedResults.map((item) => <IndeedListItem {...item} />)} //render IndeedListItem for now
                                 renderItem={item => <List.Item style={{ padding: "0 !important" }}>{item}</List.Item>}
                             /> :
-                            <h2>No jobs postings Found</h2>
+                            <h2 style={{ color: "white" }}>No Jobs Postings Found.</h2>
                         }
                     </div>
                 </Col>

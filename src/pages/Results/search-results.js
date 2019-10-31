b175a376bdd11066c82e24499fdf76edf7563a8c
John Cen <john.cen397@gmail.com>
2019-10-31 01:37:52 -0400

    Successfully passed in indeed scraped data to search results
    tested the rendering of indeed-list-item component

--- src/pages/Results/search-results.js
+++ src/pages/Results/search-results.js
@@ -1,6 +1,8 @@
+/** @jsx jsx */ import { jsx, css } from '@emotion/core'
 import React, { useState, useEffect } from 'react'
 import { Row, Col, List } from 'antd'
 import axios from 'axios'
+import * as styles from './search-results.emotion'
 import IndeedListItem from '../../components/IndeedListItem/indeed-list-item'
 
 const getIndeedJobs = async () => {
@@ -14,10 +16,10 @@ const getIndeedJobs = async () => {
 
 const SearchResults = (props) => {
     const [indeedResults, setIndeedResults] = useState([])
-    const [monsterResults, setMonsterResults] = useState([])
-    const [jobBanksResults, setJobBanksResults] = useState([])
-    const [wowJobsResults, setWowJobsResults] = useState([])
-    const [allResults, setAllResults] = useState([])
+    // const [monsterResults, setMonsterResults] = useState([])
+    // const [jobBanksResults, setJobBanksResults] = useState([])
+    // const [wowJobsResults, setWowJobsResults] = useState([])
+    // const [allResults, setAllResults] = useState([])
 
     useEffect(() => {
         const fetchIndeedJobs = async () => {
@@ -27,20 +29,24 @@ const SearchResults = (props) => {
     }, [])
 
     return (
-        <Row>
-            <Col md={{ span: 16, offset: 4 }} xs={{ span: 24 }}>
-                <h2 style={{ fontWeight: "100", marginTop: '7%', color: "white" }}>Search Results</h2>
-                {allResults ?
-                    <List
-                        split={false}
-                        size="large"
-                        dataSource={indeedResults.map((item) => <IndeedListItem {...item} />)} //render IndeedListItem for now
-                        renderItem={item => <List.Item style={{ padding: "0 !important" }}>{item}</List.Item>}
-                    /> :
-                    <h2>No jobs postings Found</h2>
-                }
-            </Col>
-        </Row>   
+        <div css={styles.SearchResultsContainer}>
+            <Row>
+                <Col md={{ span: 22, offset: 1 }} xs={{ span: 24 }}>
+                    <h2 style={{ fontWeight: "100", marginTop: '7%', color: "white" }}>Search Results</h2>
+                    <div css={styles.ResultsContent}>
+                        {indeedResults ?
+                            <List
+                                split={false}
+                                size="large"
+                                dataSource={indeedResults.map((item) => <IndeedListItem {...item} />)} //render IndeedListItem for now
+                                renderItem={item => <List.Item style={{ padding: "0 !important" }}>{item}</List.Item>}
+                            /> :
+                            <h2>No jobs postings Found</h2>
+                        }
+                    </div>
+                </Col>
+            </Row>   
+        </div>
     )
 }
 

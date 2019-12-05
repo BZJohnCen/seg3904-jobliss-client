57db04de975a1ca0302fbbc428e086288b7bb920
John Cen <john.cen397@gmail.com>
2019-12-05 13:09:58 -0500

    fixed random shuffling of results

--- src/pages/Results/search-results.js
+++ src/pages/Results/search-results.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import { Row, Col, List, Icon } from 'antd'
 import axios from 'axios'
+import Chance from 'chance'
 import * as styles from './search-results.emotion'
 import { useLocation, Link } from 'react-router-dom'
 import IndeedListItem from '../../components/IndeedListItem/indeed-list-item'
@@ -21,12 +22,15 @@ const SearchResults = (props) => {
     const [jobBanksResults, setJobBanksResults] = useState([])
     const [wowJobsResults, setWowJobsResults] = useState([])
     const [allResults, setAllResults] = useState([])
+    // const [filteredResults, setFilteredResults] = useState([])
     const [isModalHidden, setIsModalHidden] = useState(true)
+    // let [typingTimer, setTypingTimer] = useState(null)
+    const chanceSeed = new Chance(124)
     const [showSource, setShowSource] = useState({
         showIndeed: true,
-        showMonster: true,
-        showWowJobs: true,
-        showJobBank: true
+        showMonster: false,
+        showWowJobs: false,
+        showJobBank: false
     })
 
     let queryParams = useQuery()
@@ -111,93 +115,65 @@ const SearchResults = (props) => {
     }
 
     const ListResults = () => {
-        if (showSource.showIndeed && !showSource.showMonster && !showSource.showWowJobs && !showSource.showJobBank) {
-            return (
-                <List
-                    split={false}
-                    size="large"
-                    pagination={{
-                        showSizeChanger: true,
-                        onChange: (page, pageSize) => { window.scrollTo({ top: 0 }) },
-                        onShowSizeChange: (current, size) => { window.scrollTo({ top: 0 }) },
-                        showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`
-                    }}
-                    dataSource={indeedResults.map((item, i) => <IndeedListItem key={i} {...item} />)} //render IndeedListItem for now
-                    renderItem={item => <List.Item style={{ padding: "0 !important" }}>{item}</List.Item>}
-                /> 
-                        
-            )
-        } else if (!showSource.showIndeed && showSource.showMonster && !showSource.showWowJobs && !showSource.showJobBank) {
-            return (
-                <List
-                    split={false}
-                    size="large"
-                    pagination={{
-                        showSizeChanger: true,
-                        onChange: (page, pageSize) => { window.scrollTo({ top: 0 }) },
-                        onShowSizeChange: (current, size) => { window.scrollTo({ top: 0 }) },
-                        showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`
-                    }}
-                    dataSource={monsterResults.map((item, i) => <MonsterListItem key={i} {...item} />)} //render MonsterListItem for now
-                    renderItem={item => <List.Item style={{ padding: "0 !important" }}>{item}</List.Item>}
-                /> 
-            )
-        } else if (!showSource.showIndeed && !showSource.showMonster && showSource.showWowJobs && !showSource.showJobBank) {            
-            return (
-                <List
-                    split={false}
-                    size="large"
-                    pagination={{
-                        showSizeChanger: true,
-                        onChange: (page, pageSize) => { window.scrollTo({ top: 0 }) },
-                        onShowSizeChange: (current, size) => { window.scrollTo({ top: 0 }) },
-                        showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`
-                    }}
-                    dataSource={wowJobsResults.map((item, i) => <WowJobsListItem key={i} {...item} />)} //render IndeedListItem for now
-                    renderItem={item => <List.Item style={{ padding: "0 !important" }}>{item}</List.Item>}
-                /> 
-            )
-        } else if (!showSource.showIndeed && !showSource.showMonster && !showSource.showWowJobs && showSource.showJobBank) {
-            return (
-                <List
-                    split={false}
-                    size="large"
-                    pagination={{
-                        showSizeChanger: true,
-                        onChange: (page, pageSize) => { window.scrollTo({ top: 0 }) },
-                        onShowSizeChange: (current, size) => { window.scrollTo({ top: 0 }) },
-                        showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`
-                    }}
-                    dataSource={jobBanksResults.map((item, i) => <JobBanksListItem key={i} {...item} />)} //render IndeedListItem for now
-                    renderItem={item => <List.Item style={{ padding: "0 !important" }}>{item}</List.Item>}
-                /> 
-            )
-        } else if (showSource.showIndeed && showSource.showMonster && showSource.showWowJobs && showSource.showJobBank) {
-            return (
-                <List
-                    split={false}
-                    size="large"
-                    pagination={{
-                        showSizeChanger: true,
-                        onChange: (page, pageSize) => { window.scrollTo({ top: 0 }) },
-                        onShowSizeChange: (current, size) => { window.scrollTo({ top: 0 }) },
-                        showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`
-                    }}
-                    dataSource={[...indeedResults, ...monsterResults, ...wowJobsResults, ...jobBanksResults].sort(() => Math.random() - 0.5).map((job, i) => {
-                        if (job.url.includes("indeed")){
-                            return <IndeedListItem key={i} {...job} />
-                        } else if (job.url.includes("monster")) {
-                            return <MonsterListItem key={i} {...job} />
-                        } else if (job.url.includes("jobbank")) {
-                            return <JobBanksListItem key={i} {...job} />
-                        } else {
-                            return <WowJobsListItem key={i} {...job} />
-                        }
-                    })}
-                    renderItem={item => <List.Item style={{ padding: "0 !important" }}>{item}</List.Item>}
-                /> 
-            )
+        let result = []
+        //base four filters
+        if (showSource.showIndeed && !showSource.showMonster && !showSource.showWowJobs && !showSource.showJobBank){ //indeed only
+            result = [...indeedResults]            
+        } else if (!showSource.showIndeed && showSource.showMonster && !showSource.showWowJobs && !showSource.showJobBank){ //monster only
+            result = [...monsterResults]
+        } else if (!showSource.showIndeed && !showSource.showMonster && showSource.showWowJobs && !showSource.showJobBank){ //wowjobs only
+            result = [...wowJobsResults]
+        } else if (!showSource.showIndeed && !showSource.showMonster && !showSource.showWowJobs && showSource.showJobBank){ //jobbanks only
+            result = [...jobBanksResults]
+        } else if (showSource.showIndeed && showSource.showMonster && !showSource.showWowJobs && !showSource.showJobBank){ //combinations: indeed + monster
+            result = chanceSeed.shuffle([...indeedResults, ...monsterResults])
+        } else if (showSource.showIndeed && !showSource.showMonster && showSource.showWowJobs && !showSource.showJobBank){ //indeed + wowjobs
+            result = chanceSeed.shuffle([...indeedResults, ...wowJobsResults])
+        } else if (showSource.showIndeed && !showSource.showMonster && !showSource.showWowJobs && showSource.showJobBank){ //indeed + jobbanks
+            result = chanceSeed.shuffle([...indeedResults, ...jobBanksResults])
+        } else if (showSource.showIndeed && showSource.showMonster && showSource.showWowJobs && !showSource.showJobBank){ //indeed + monster + wowjobs
+            result = chanceSeed.shuffle([...indeedResults, ...monsterResults, ...wowJobsResults])
+        } else if (showSource.showIndeed && showSource.showMonster && !showSource.showWowJobs && showSource.showJobBank){ //indeed + monster +jobbanks
+            result = chanceSeed.shuffle([...indeedResults, ...monsterResults, ...jobBanksResults])
+        } else if (showSource.showIndeed && !showSource.showMonster && showSource.showWowJobs && showSource.showJobBank){ //indeed + wowjobs + jobbanks
+            result = chanceSeed.shuffle([...indeedResults, ...wowJobsResults, ...jobBanksResults])
+        } else if (!showSource.showIndeed && showSource.showMonster && showSource.showWowJobs && !showSource.showJobBank){ //monster + wowjobs
+            result = chanceSeed.shuffle([...monsterResults, ...wowJobsResults])
+        } else if (!showSource.showIndeed && showSource.showMonster && !showSource.showWowJobs && showSource.showJobBank){ //monster + jobbanks
+            result = chanceSeed.shuffle([...monsterResults, ...jobBanksResults])
+        } else if (!showSource.showIndeed && showSource.showMonster && showSource.showWowJobs && showSource.showJobBank){ //monster + wowjobs + jobbanks
+            result = chanceSeed.shuffle([...monsterResults, ...wowJobsResults, ...jobBanksResults])
+        } else if (!showSource.showIndeed && !showSource.showMonster && showSource.showWowJobs && showSource.showJobBank){ //wowjobs + jobbanks
+            result = chanceSeed.shuffle([...wowJobsResults, ...jobBanksResults])
+        } else if (showSource.showIndeed && showSource.showMonster && showSource.showWowJobs && showSource.showJobBank){ //all true, in order
+            result = [...indeedResults, ...monsterResults, ...wowJobsResults, ...jobBanksResults]
+        } else if (!showSource.showIndeed && !showSource.showMonster && !showSource.showWowJobs && !showSource.showJobBank){ // all false, mixed
+            result = chanceSeed.shuffle([...indeedResults, ...monsterResults, ...wowJobsResults, ...jobBanksResults])
         }
+      
+        return result.length !== 0 ?
+            <List
+                split={false}
+                size="large"
+                pagination={{
+                    showSizeChanger: true,
+                    onChange: (page, pageSize) => { window.scrollTo({ top: 0 }) },
+                    onShowSizeChange: (current, size) => { window.scrollTo({ top: 0 }) },
+                    showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`
+                }}
+                dataSource={result.map((job, i) => {
+                    if (job.url.includes("indeed")){
+                        return <IndeedListItem key={i} {...job} />
+                    } else if (job.url.includes("monster")) {
+                        return <MonsterListItem key={i} {...job} />
+                    } else if (job.url.includes("jobbank")) {
+                        return <JobBanksListItem key={i} {...job} />
+                    } else {
+                        return <WowJobsListItem key={i} {...job} />
+                    }
+                })}
+                renderItem={item => <List.Item style={{ padding: "0 !important" }}>{item}</List.Item>}
+            /> : <h2 style={{ color: "white" }}>Loading...</h2>
     }
 
     return (
@@ -209,7 +185,7 @@ const SearchResults = (props) => {
                             <svg width="60" height="35" viewBox="1 0 22 24" fill="white" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm-4.828 11.5l4.608 3.763-.679.737-6.101-5 6.112-5 .666.753-4.604 3.747h11.826v1h-11.828z"/></svg>
                         </Link>
                         <h2 style={{ fontWeight: "100", marginTop: '7%', color: "white" }}>Search Results</h2>
-                        <button onClick={showModal} css={styles.FilterBtn}>Filter</button>
+                        <button onClick={showModal} css={styles.FilterBtn}>Filter</button>                        
                     </div>
                     <FilterModal
                         isHidden={isModalHidden}
@@ -218,10 +194,7 @@ const SearchResults = (props) => {
                         showSource={showSource}
                     />
                     <div css={styles.ResultsContent}>
-                        {                        
-                            ListResults()   
-                            // <h2 style={{ color: "white" }}>No Jobs Postings Found.</h2>
-                        }
+                        {ListResults()}
                     </div>
                 </Col>
             </Row>   
